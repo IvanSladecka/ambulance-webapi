36a852a1f281a257864f18e1e18c8dfc975198b4
Ivan Sladecka <ivan.sladecka@siemens-healthineers.com>
2024-04-08 14:25:06 +0200

    Added custom metrics

--- cmd/ambulance-api-service/main.go
+++ cmd/ambulance-api-service/main.go
@@ -6,47 +6,99 @@ import (
 	"os"
 	"strings"
 	"time"
+    "net/http"
 
 	"github.com/IvanSladecka/ambulance-webapi/api"
 	"github.com/IvanSladecka/ambulance-webapi/internal/ambulance_wl"
 	"github.com/IvanSladecka/ambulance-webapi/internal/db_service"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-    "github.com/gin-contrib/cors"
+
+    "github.com/prometheus/client_golang/prometheus/promhttp"
+    "github.com/technologize/otel-go-contrib/otelginmetrics"
+    "go.opentelemetry.io/otel"
+    "go.opentelemetry.io/otel/attribute"
+    "go.opentelemetry.io/otel/exporters/prometheus"
+    "go.opentelemetry.io/otel/sdk/metric"
+    "go.opentelemetry.io/otel/sdk/resource"
+    semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 )
 
 func main() {
-    log.Printf("Server started")
-    port := os.Getenv("AMBULANCE_API_PORT")
-    if port == "" {
-        port = "8080"
-    }
-    environment := os.Getenv("AMBULANCE_API_ENVIRONMENT")
-    if !strings.EqualFold(environment, "production") { // case insensitive comparison
-        gin.SetMode(gin.DebugMode)
-    }
-    engine := gin.New()
-    engine.Use(gin.Recovery())
+	log.Printf("Server started")
+	port := os.Getenv("AMBULANCE_API_PORT")
+	if port == "" {
+		port = "8080"
+	}
+	environment := os.Getenv("AMBULANCE_API_ENVIRONMENT")
+	if !strings.EqualFold(environment, "production") { // case insensitive comparison
+		gin.SetMode(gin.DebugMode)
+	}
+	engine := gin.New()
+	engine.Use(gin.Recovery())
 
-    corsMiddleware := cors.New(cors.Config{
-        AllowOrigins:     []string{"*"},
-        AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "PATCH"},
-        AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
-        ExposeHeaders:    []string{""},
-        AllowCredentials: false,
-        MaxAge: 12 * time.Hour,
-      })
-    engine.Use(corsMiddleware)
+    // setup telemetry
+    initTelemetry()
 
-    // setup context update  middleware
-    dbService := db_service.NewMongoService[ambulance_wl.Ambulance](db_service.MongoServiceConfig{})
-    defer dbService.Disconnect(context.Background())
-    engine.Use(func(ctx *gin.Context) {
-        ctx.Set("db_service", dbService)
-        ctx.Next()
+    engine.Use(otelginmetrics.Middleware(
+        "Ambulance WebAPI Service",
+        // Custom attributes
+        otelginmetrics.WithAttributes(func(serverName, route string, request *http.Request) []attribute.KeyValue {
+            return append(otelginmetrics.DefaultAttributes(serverName, route, request))
+        }),
+    ))
+
+	corsMiddleware := cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{""},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	})
+	engine.Use(corsMiddleware)
+
+	// setup context update  middleware
+	dbService := db_service.NewMongoService[ambulance_wl.Ambulance](db_service.MongoServiceConfig{})
+	defer dbService.Disconnect(context.Background())
+	engine.Use(func(ctx *gin.Context) {
+		ctx.Set("db_service", dbService)
+		ctx.Next()
+	})
+
+	// request routings
+	ambulance_wl.AddRoutes(engine)
+	engine.GET("/openapi", api.HandleOpenApi)
+
+    // metrics endpoint
+    promhandler := promhttp.Handler()
+    engine.Any("/metrics", func(ctx *gin.Context) {
+        promhandler.ServeHTTP(ctx.Writer, ctx.Request)
     })
 
-    // request routings
-    ambulance_wl.AddRoutes(engine)
-    engine.GET("/openapi", api.HandleOpenApi)
-    engine.Run(":" + port)
+	engine.Run(":" + port)
 }
+
+// initialize OpenTelemetry instrumentations
+func initTelemetry() error {
+    ctx := context.Background()
+    res, err := resource.New(ctx,
+     resource.WithAttributes(semconv.ServiceNameKey.String("Ambulance WebAPI Service")),
+     resource.WithAttributes(semconv.ServiceNamespaceKey.String("WAC Hospital")),
+     resource.WithSchemaURL(semconv.SchemaURL),
+     resource.WithContainer(),
+    )
+  
+    if err != nil {
+     return err
+    }
+  
+    metricExporter, err := prometheus.New()
+    if err != nil {
+     return err
+    }
+  
+    metricProvider := metric.NewMeterProvider(metric.WithReader(metricExporter), metric.WithResource(res))
+    otel.SetMeterProvider(metricProvider)
+    return nil
+  }
